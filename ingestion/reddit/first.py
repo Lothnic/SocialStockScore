3ed369edc4af5c10830f11d7d40c4c2df45cbd64
Mayank Joshi <joshimayank08012007@gmail.com>
2025-07-03 16:47:05 +0000

    completed first version of reddit scoring

--- ingestion/reddit/first.py
+++ ingestion/reddit/first.py
@@ -30,8 +30,43 @@ def fetch_user_data(username):
             'created_utc': post.created_utc,
             'score': post.score
         })
+
     return comments, posts
 
-fetch_profile = fetch_user_data(username)
-print(f"Comments: {fetch_profile[0]}")
-print(f"Posts: {fetch_profile[1]}")
+def subreddit_diversity(username):
+    reddit_user = reddit.redditor(username)
+    subreddits = set()
+
+    for comment in reddit_user.comments.new(limit=50):
+        subreddits.add(comment.subreddit.display_name)
+
+    for post in reddit_user.submissions.new(limit=50):
+        subreddits.add(post.subreddit.display_name)
+
+    return len(subreddits)
+
+def total_karma(username):
+    reddit_user = reddit.redditor(username)
+    return reddit_user.comment_karma + reddit_user.link_karma
+
+def cal_score(username, comments, posts):
+    score = 0
+
+    karma = total_karma(username)
+    score += min(karma/1000 * 8, 8) #karma score
+
+    diversity = subreddit_diversity(username)
+    score += min(diversity/10 * 4, 4) #subreddits diversity score
+
+    avg_length = sum(len(c['body']) for c in comments) / len(comments)
+    if avg_length > 100: score += 4
+    elif avg_length > 50: score += 2 #comment length score
+
+    toxic_words = ["r*pe", "hate", "cringe", "simp", "incel", "sigma", "elon"]
+    toxic_count = sum(any(word in c['body'].lower() for word in toxic_words) for c in comments)
+    score -= min(toxic_count, 5) #profanity score
+
+    return round(score, 2)
+
+final_score = cal_score(username, *fetch_user_data(username))
+print(f"Reddit user {username} has a score of {final_score}/20")
