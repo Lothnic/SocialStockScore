3298fd5bf63c8ae1b80a82eec84d3e2f98147405
Mayank Joshi <joshimayank08012007@gmail.com>
2025-07-06 18:29:28 +0000

    Update linkedin

--- ingestion/linkedin/first.py
+++ ingestion/linkedin/first.py
@@ -1,6 +1,7 @@
 from linkedin_api import Linkedin
 from dotenv import load_dotenv
 import os
+import pprint
 
 # Load environment variables from .env file
 load_dotenv()
@@ -9,5 +10,8 @@ password = os.getenv('LINKEDIN_PASS')
 api = Linkedin('thehorizondude@gmail.com', password)
 
 # GET a profile
-profile = api.get_profile('mayankjoshi0801')
-print(profile)
+# profile = api.get_profile('mayankjoshi0801')
+
+posts = api.get_profile_posts('mayankjoshi0801')
+
+pprint.pprint(posts)
