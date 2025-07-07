e16d8fbd0599fa5c2565cbaccf302fcf5419c5d7
Mayank Joshi <joshimayank08012007@gmail.com>
2025-07-07 08:15:49 +0000

    fixed the json errors in linkedin data fetching

--- ingestion/linkedin/first.py
+++ ingestion/linkedin/first.py
@@ -2,6 +2,8 @@ from linkedin_api import Linkedin
 from dotenv import load_dotenv
 import os
 import pprint
+import json
+
 
 # Load environment variables from .env file
 load_dotenv()
@@ -10,8 +12,13 @@ password = os.getenv('LINKEDIN_PASS')
 api = Linkedin('thehorizondude@gmail.com', password)
 
 # GET a profile
-# profile = api.get_profile('mayankjoshi0801')
+profile = api.get_profile('mayankjoshi0801')
+print(json.dumps(profile['headline'], indent=2, ensure_ascii=False))
 
-posts = api.get_profile_posts('mayankjoshi0801')
 
-pprint.pprint(posts)
+
+# posts = api.get_profile_posts('mayankjoshi0801')
+
+# for i in range(len(posts)):
+#     post = posts[i]
+#     pprint.pprint(json.dumps(post['commentary']['text']['text'], indent=2, ensure_ascii=False))
